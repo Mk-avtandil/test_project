497e1c4f17465611779baf8db1c5d2c20a7ed1a0
bby2r <baiturbulanbekov@gmail.com>
2024-09-27 19:02:12 +0600

    Упростил код в ProductController

--- app/Http/Controllers/Twill/ProductController.php
+++ app/Http/Controllers/Twill/ProductController.php
@@ -24,13 +24,42 @@ class ProductController extends BaseModuleController
     protected $titleFormKey = 'type';
     protected $titleFormLabel = 'Type';
 
-
+    private static array $formFields;
     /**
      * This method can be used to enable/disable defaults. See setUpController in the docs for available options.
      */
     protected function setUpController(): void
     {
         $this->disablePermalink();
+        self::$formFields = [
+            Input::make()
+                ->name('type')
+                ->label('Тип продукта')
+                ->maxLength(100)
+                ->required(),
+            Wysiwyg::make()
+                ->name('description')
+                ->label('Описание')
+                ->required()
+                ->maxLength(1000),
+            Input::make()
+                ->name('color')
+                ->required()
+                ->label('Цвет'),
+            Input::make()
+                ->name('size')
+                ->maxLength(100)
+                ->required()
+                ->type('number')
+                ->label('Размер'),
+            Input::make()
+                ->name('price')
+                ->type('number')
+                ->required()
+                ->label('Цена'),
+            Checkbox::make()
+                ->name('is_in_stock')
+        ];
     }
 
     /**
@@ -41,85 +70,16 @@ class ProductController extends BaseModuleController
     {
         $form = parent::getForm($model);
 
-        $form->add(
-            Input::make()
-                ->name('type')
-                ->label('Тип продукта')
-                ->maxLength(100)
-                ->required(),
-        );
-
-        $form->add(
-            Wysiwyg::make()
-                ->name('description')
-                ->label('Описание')
-                ->required()
-                ->maxLength(1000),
-        );
-
-        $form->add(
-            Input::make()
-                ->name('color')
-                ->required()
-                ->label('Цвет'),
-        );
-
-        $form->add(
-            Input::make()
-                ->name('size')
-                ->maxLength(100)
-                ->required()
-                ->type('number')
-                ->label('Размер'),
-        );
-
-        $form->add(
-            Input::make()
-                ->name('price')
-                ->type('number')
-                ->required()
-                ->label('Цена'),
-        );
-
-        $form->add(
-            Checkbox::make()
-                ->name('is_in_stock')
-        );
+        foreach (self::$formFields as $field) {
+            $form->add($field);
+        }
 
         return $form;
     }
 
     public function getCreateForm(): Form
     {
-        return Form::make([
-            Input::make()
-                ->name('type')
-                ->label('Тип продукта')
-                ->maxLength(100)
-                ->required(),
-            Wysiwyg::make()
-                ->name('description')
-                ->label('Описание')
-                ->required()
-                ->maxLength(1000),
-            Input::make()
-                ->name('color')
-                ->required()
-                ->label('Цвет'),
-            Input::make()
-                ->name('size')
-                ->maxLength(100)
-                ->required()
-                ->type('number')
-                ->label('Размер'),
-            Input::make()
-                ->name('price')
-                ->type('number')
-                ->required()
-                ->label('Цена'),
-            Checkbox::make()
-                ->name('is_in_stock')
-        ]);
+        return Form::make(self::$formFields);
     }
 
 
